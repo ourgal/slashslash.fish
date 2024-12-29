b4fc8a8f1ca281ea8438ef96c90cd4b87facf7f2
danzimm <daniel.zimmerman@me.com>
2024-12-29 15:55:27 -0600

    Cleanup

--- conf.d/slashslash/git_plugin.fish
+++
@@ -1,6 +0,0 @@
-type -q git; or return 1
-set root (git rev-parse --show-toplevel 2>/dev/null); or return 1
-
-function __slashslash_root --description 'Lookup git root' --inherit-variable root
-  echo "$root"
-end
