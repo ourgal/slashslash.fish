334d0ca8505bc785ae8f2875fbcaf8d6217efa27
danzimm <daniel.zimmerman@me.com>
2024-12-29 13:33:30 -0600

    Rewrite PWD hook

--- functions/__slashslash_pwd_hook.fish
+++ functions/__slashslash_pwd_hook.fish
@@ -1,26 +1,18 @@
 function __slashslash_pwd_hook --on-variable PWD --description '// PWD change hook'
   status --is-command-substitution; and return
-  set -q NO_SLASHSLASH; and return
-  set -q slashslash_plugins; or return
+  set -qg NO_SLASHSLASH; and return
+  set -qg __slashslash_plugins; or return
 
   # Reset to start
-  functions -e __slashslash_root __slashslash_resolve_cell __slashslash_resolve_subpath __slashslash_plugin_complete
-  set -e __slashslash_plugin
-
-  for plugin in $slashslash_plugins
-    set -q __slashslash_verbose; and echo "Loading $plugin"
-    if not functions -q "__slashslash_load_$plugin"
-      set -q __slashslash_verbose; and echo "W: No plugin loader for $plugin"
-      continue
-    end
-    if __slashslash_load_$plugin
-      set -q __slashslash_verbose; and echo "Success!"
-      if functions -q __slashslash_root
-        set -g __slashslash_plugin "$plugin"
-        break
-      else
-        echo "W: slashslash plugin succeeded loading, but didn't define __slashslash_root: $plugin" >&2
-      end
-    end
+  for plugin in $__slashslash_plugins
+    set -l function_key __slashslash_plugin_$name
+    set -qg $function_key; or continue
+    set -af plugin_funcs $$function_key
   end
+
+  fish -c "set -l cells
+for func in $plugin_funcs
+  set -a cells (func)
+end
+__slashslash_write_cells $fish_pid \$cells" &
 end
