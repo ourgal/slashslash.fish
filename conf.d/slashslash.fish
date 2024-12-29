3d9e0df5aca61a69b0fe70d1327ab1b83aa4d371
danzimm <daniel.zimmerman@me.com>
2024-12-29 14:45:38 -0600

    enableothers

--- conf.d/slashslash.fish
+++ conf.d/slashslash.fish
@@ -6,9 +6,9 @@ alias ss "eval"
 # Setup defaults: user can disable with slashslash -d|--disable CMD
 slashslash ss cat ls cp rm mv cd zip unzip vim nvim vi buck sl git hg grep ack
 
-#slashslash_plugin buck __slashslash_buck
+slashslash_plugin buck __slashslash_buck
 slashslash_plugin git __slashslash_git
-#slashslash_plugin hg __slashslash_hg
+slashslash_plugin hg __slashslash_hg
 
 # Call initial reload to setup event handlers
 slashslash_cells --reload
