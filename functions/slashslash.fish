694449b0bafe3fc438ba0a4b09ffee3b158c5930
danzimm <daniel.zimmerman@me.com>
2024-12-29 15:58:03 -0600

    Move slashslash_cells into slashslash

--- functions/slashslash.fish
+++ functions/slashslash.fish
@@ -245,3 +245,36 @@ function __slashslash_complete_cmd -a cur --description "Print completions for a
   end
   return 0
 end
+
+function __slashslash_cells_cmd --description "Query the currently available cells. Pass -r/--reload to reload"
+  argparse r/reload h/help -- $argv; or return
+
+  if set -ql _flag_h
+    echo "Usage:"
+    echo "slashslash cells [-h|--help]"
+    echo "slashslash cells [-r|reload]"
+    echo
+    echo "Prints the currently available cells. Passing -r|--reload force reloads the cells"
+    return 0
+  end
+
+  if set -ql _flag_r
+    __slashslash_load_cells --reset
+    __slashslash_pwd_hook
+    return $status
+  end
+
+  if not set -qg __slashslash_current_cells; or not set -qg __slashslash_current_cell_paths
+    return 0
+  end
+  set -f n (count $__slashslash_current_cells)
+  if test $n -ne (count $__slashslash_current_cell_paths)
+    echo "Internal error: mismatch number of cells and paths" >&2
+    return 1
+  end
+  test $n -eq 0; and return 0
+
+  for i in (seq 1 $n)
+    echo $__slashslash_current_cells[$i] : $__slashslash_current_cell_paths[$i]
+  end
+end
