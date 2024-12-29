3c09ed1c9ab6d1d434dc9bb8efd2c5d79ef3f2bb
danzimm <daniel.zimmerman@me.com>
2024-12-29 13:47:39 -0600

    Fix expand

--- functions/__slashslash_expand.fish
+++ functions/__slashslash_expand.fish
@@ -1,10 +1,13 @@
 function __slashslash_expand --description "Expand // based on current directory"
-  if not set -qg __slashslash_current_cell_names; or not set -qg __slashslash_current_cell_paths
+  if not set -qg __slashslash_current_cells; or not set -qg __slashslash_current_cell_paths
+    set -qg __slashslash_verbose; and echo "No loaded cells" >&2
     string join \n -- $argv
     return 0
   end
 
   for arg in $argv
+    set -qg __slashslash_verbose; and echo "processing $arg" >&2
+
     # Is there // somewhere in this arg?
     if not string match -rq '^(?<cell>[^/\s]*)//(?<subpath>[^\s]*)$' -- $arg
       echo "$arg"
@@ -16,13 +19,16 @@ function __slashslash_expand --description "Expand // based on current directory
       set cell "//"
     end
 
-    if not set idx (contains -i "$cell" -- $__slashslash_current_cell_names)
+    if not set idx (contains -i "$cell" $__slashslash_current_cells)
       echo "$arg"
       continue
     end
+    set -qg __slashslash_verbose; and echo "idx=$idx" >&2
 
     set -l root $__slashslash_current_cell_paths[$idx]
+    set -qg __slashslash_verbose; and echo "root=$root" >&2
     set -l abs "$root/$subpath"
+    set -qg __slashslash_verbose; and echo "abs=$abs" >&2
 
     echo -n (realpath -s --relative-to=. "$abs")
     # Use abs instead of $subpath so that when $subpath is empty
