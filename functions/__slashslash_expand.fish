ced24e9a028e4ca23a12734868870cb4cd1d6d5a
danzimm <daniel.zimmerman@me.com>
2024-12-29 13:39:56 -0600

    Rewrite __expand based on new plugin system

--- functions/__slashslash_expand.fish
+++ functions/__slashslash_expand.fish
@@ -1,8 +1,9 @@
 function __slashslash_expand --description "Expand // based on current directory"
-  if not functions -q __slashslash_root
-    string join -- \n $argv
-    return
+  if not set -qg __slashslash_current_cell_names; or not set -qg __slashslash_current_cell_paths
+    string join \n -- $argv
+    return 0
   end
+
   for arg in $argv
     # Is there // somewhere in this arg?
     if not string match -rq '^(?<cell>[^/\s]*)//(?<subpath>[^\s]*)$' -- $arg
@@ -10,36 +11,19 @@ function __slashslash_expand --description "Expand // based on current directory
       continue
     end
 
-    # Ok there is, now to actually expand cell//
-    if test -n "$cell"
-      set -f cached_key "cached_cell_$cell"
-      if set -q "$cached_key"
-        set -f root "$$cached_key"
-      else if functions -q __slashslash_resolve_cell
-        if set -f root (__slashslash_resolve_cell "$cell")
-          set -f "$cached_key" "$root"
-        else
-          set -e root  # unset root to fall through to below
-        end
-      end
-    end
-    if not set -q root
-      if set -q cached_root
-        set -f root "$cached_root"
-      else if not set -f root (__slashslash_root)
-        echo "$arg"
-        continue
-      else
-        set -f cached_root "$root"
-      end
+    if test -z "$cell"
+      # Default to 'root'
+      set cell "//"
     end
 
-    if functions -q __slashslash_resolve_subpath
-      # This is to e.g. expand foo:bar.bzl > foo/bar.bzl
-      set -f subpath (__slashslash_resolve_subpath "$subpath")
+    if not set idx (contains -i "$cell" -- $__slashslash_current_cell_names)
+      echo "$arg"
+      continue
     end
 
+    set -l root $__slashslash_current_cell_paths[$idx]
     set -l abs "$root/$subpath"
+
     echo -n (realpath -s --relative-to=. "$abs")
     # Use abs instead of $subpath so that when $subpath is empty
     # the match succeeds and we correctly get a trailing slash.
