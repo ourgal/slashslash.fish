1376679b7cc35b8affcde5b52191115ee7ffbb6e
danzimm <daniel.zimmerman@me.com>
2024-12-29 13:34:54 -0600

    Change startup code to use new plugin mechanism

--- conf.d/slashslash.fish
+++ conf.d/slashslash.fish
@@ -4,17 +4,5 @@ alias ss "eval"
 # Setup defaults: user can disable with slashslash -d|--disable CMD
 slashslash ss cat ls cp rm mv cd zip unzip vim nvim vi buck sl git hg grep ack
 
-set -g slashslash_plugins
-
-for path in ~/.config/fish/conf.d/slashslash/*_plugin.fish
-  string match -rq "(?<name>[^/]+)_plugin.fish" -- $path; or continue
-  function __slashslash_load_$name --description "Load slashslash plugin $name from $path" --inherit-variable path
-    source $path
-  end
-  set -a slashslash_plugins "$name"
-end
-
-slashslash -p buck git hg
-
-# Call the hook to setup the event listener & check if initial PWD is inside a //
-__slashslash_pwd_hook
+# Call initial reload to setup event handlers
+slashslash_cells --reload
